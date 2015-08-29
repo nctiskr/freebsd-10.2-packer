02bae4b44b35f422e0f0fbdebac25be5e543e775
nctiskr <nctiskr@yandex.com>
2015-08-29 11:04:44 +0300

    FreeBSD 10.2 Vagrant Box build with Packer

---
+++ scripts/update.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+
+# Packages which are pre-installed
+INSTALLED_PACKAGES="virtualbox-ose-additions bash sudo ca_root_nss"
+
+# Configuration files
+HOSTS=""
+MAKE_CONF=""
+LOADER_CONF=""
+RC_CONF=""
+RESOLV_CONF=""
+
+# Private key of Vagrant (you probable don't want to change this)
+VAGRANT_PRIVATE_KEY="https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub"
+
+echo '==> Make network accessible...'
+echo 'nameserver 8.8.8.8' > /etc/resolv.conf
+echo 'nameserver 8.8.4.4' >> /etc/resolv.conf
+
+echo '==> Configuring system...'
+sysrc hostname=vagrant
+sysrc ifconfig_em0=DHCP
+
+echo '==> Configuring SSH...'
+sysrc sshd_enable=YES
+sed -i '' -e 's/#PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i '' -e 's/#UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
+
+echo '==> Setting up pkg...'
+if [ ! -f /usr/local/sbin/pkg ]; then
+    env ASSUME_ALWAYS_YES=yes pkg bootstrap
+fi
+
+echo '==> Update pkg...'
+pkg update
+pkg upgrade -y
+
+echo '==> Install required packages...'
+for p in $INSTALLED_PACKAGES; do
+    pkg install -y "$p"
+done
+
+echo '==> Create the vagrant user...'
+pw useradd -n vagrant -s /usr/local/bin/bash -m -G wheel -h 0 <<EOP
+vagrant
+EOP
+
+echo '==> Enable sudo for this user...'
+echo "%vagrant ALL=(ALL) NOPASSWD: ALL" >> /usr/local/etc/sudoers
+
+echo '==> Install authorized ssh keys...'
+mkdir /home/vagrant/.ssh
+fetch -o /home/vagrant/.ssh/authorized_keys $VAGRANT_PRIVATE_KEY
+chown vagrant:vagrant /home/vagrant/.ssh/authorized_keys
+chown vagrant:vagrant /home/vagrant/.ssh
+chmod -R go-rwx /home/vagrant/.ssh
+
+# Fetching Configuration Files
+echo '==> Fetching hosts file...'
+fetch -o /etc/make.conf $HOSTS
+
+echo '==> Fetching make.conf...'
+fetch -o /etc/make.conf $MAKE_CONF
+
+echo '==> Fetching loader.conf...'
+fetch -o /boot/loader.conf $LOADER_CONF
+
+echo '==> Fetching rc.conf...'
+fetch -o /etc/rc.conf $RC_CONF
+
+echo '==> Fetching resolv.conf...'
+fetch -o /etc/resolv.conf $RESOLV_CONF
+
+echo '==> Clean up installed packages...'
+pkg clean -a -y
+
+echo '==> Remove the history...'
+cat /dev/null > /root/.history
+
+echo '==> Try to make it even smaller...'
+dd if=/dev/zero of=/tmp/ZEROES bs=1M
+
+echo '==> Empty out tmp directory...'
+rm -rf /tmp/*
+
+echo '==> Done.'
---
+++ scripts/zfsinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo '==> Installing FreeBSD with ZFS root...'
+mount_cd9660 /dev/cd0 /cdrom
+
+echo '==> VirtualBox detected, installing FreeBSD on ada0.'
+zfsinstall -d ada0 -s 2G -u /cdrom/10.2-RELEASE-amd64
+
+echo '==> Preparing machine for first boot via setup script...'
+mount -t devfs devfs /mnt/dev
+cat update.sh | chroot /mnt sh -
